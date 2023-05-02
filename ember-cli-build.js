75e6e07d6bed48a3efc11550a0c8b2ecea56ec31
klickers <bluebirid228@gmail.com>
2023-05-02 14:56:18 -0700

    add tailwindcss + sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,29 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // Add options here
-  });
+    const app = new EmberApp(defaults, {
+        postcssOptions: {
+            compile: {
+                extension: "scss",
+                enabled: true,
+                parser: require("postcss-scss"),
+                plugins: [
+                    {
+                        module: require("autoprefixer"),
+                        options: {},
+                    },
+                    {
+                        module: require("tailwindcss"),
+                        options: {
+                            config: "./tailwind.config.js",
+                        },
+                    },
+                ],
+            },
+        },
+    });
 
-  return app.toTree();
+    return app.toTree();
 };
